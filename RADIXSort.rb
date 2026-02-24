8fc5f864ebf03f4a629852b07a0d76dbafc145c3
WizziWizBiz <xavierwambach@gmail.com>
2026-02-24 12:46:56 -0600

    implemented the sorting part of the radix sort, still needs to be tested more extensively

--- RADIXSort.rb
+++ RADIXSort.rb
@@ -6,12 +6,33 @@ def radix(teams, metric)
     # each team gets a tuple with the metric * 100 and then floored to handle decimal values
     teamsWithMetric << [teams[i], (100 * (teams[i].send(metric) || 0)).floor]
   end  
+
   #SORTING
+  # initialize buckets and begin with all elements in first bucket
+  oldBuckets = Array.new(10) { [] }
+  oldBuckets[0] = teamsWithMetric
+  digit = 1
+  while digit <=5
+    newBuckets = Array.new(10) { [] }
+    # for every element in every bucket
+    oldBuckets.each do |bucket|
+      bucket.each do |element|
+        # extract current digit and put element in corresponding bucket
+        currDigit = element[1] % 10
+        element[1] = element[1] / 10
+        newBuckets[currDigit] << element
+      end
+    end
+    # update buckets for next loop
+    oldBuckets = newBuckets
+    digit = digit + 1
+  end  
 
   #GETTING RETURN
   sortedTeams = []
   teamsWithMetric.each_index do |i|
     sortedTeams[i] = teamsWithMetric[i][0]
+    #equality handling
     if (i>0)
       if (teamsWithMetric[i][1] == teamsWithMetric[i-1][1]) and 
          (sortedTeams[i].number < sortedTeams[i-1].number)
@@ -21,4 +42,3 @@ def radix(teams, metric)
   end
   return sortedTeams
 end
-
--- Tester_RADIX.rb
+++ Tester_RADIX.rb
@@ -5,7 +5,7 @@ team2823 = Robot.new(2823, "The Automatons")
 team2491 = Robot.new(2491, "No Mythic")
 teams = [team2823, team2491]
 teams[0].opr = 28.23
-teams[1].opr = 28.23
+teams[1].opr = 24.91
 teamsNum = radix(teams, :number)
 teamsNum.each do |team|
   puts (team.name + ": " + team.number.to_s)
