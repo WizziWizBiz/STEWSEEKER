cd87e4d3f97f49bff09f0037b021b231a9bc6cfd
WizziWizBiz <xavierwambach@gmail.com>
2026-04-20 12:42:57 -0500

    Moved to using Built-in hash to handle the hash map, implemented a few functions that work with it: lookup (may be deleted) hashMapRemove and the exception NodeDoesntExist started on larger processes for adding and removing from the heap structure

--- SelectionHeap.rb
+++ SelectionHeap.rb
@@ -1,54 +1,8 @@
 require_relative "Robot"
-class SelectionHeap
-  
-  hashmap = Array.new[100000] #HOW TO DO HASHMAP?????
-  heapRoot = nil
-  metric = nil
-  
-  #metric to organize the heap by and teams (sorted by number?) are expected to create the heap from
-  def initialize(metric, teams)
-    @metric = metric
-    teams.each_index do |i|
-      node = HeapNode.new(teams[i], @metric) 
-      # add node to heap (and to hashmap but thats probably going to be done in the heap add function)
-    end
-  end
+require "Hash"
 
-  # removes a node fron heap and hash map, returning its value
-  def removeNode(number)
-    return nil
-  end
-
-  private
-  # Given a team number, finds the corresponding node in the hash map and returns it. returns nil if not found
-  def lookup(number)
-    return nil
-  end
-
-  # adds a node to the hash map and heap
-  def addNode(node)
-    return nil
-  end
-  # helper function to handle adding things to the hash map
-  def hashMapAdd(node) 
-    return nil
-  end
-  # helper function to handle removing things from the hashmap
-  def hashMapRemove(number)
-    return nil
-  end
-
-  # reheapification for adding the given node
-  def reheapifyUp(node)
-    return nil
-  end
-
-  #reheapification for removing the given node
-  def reheapifyDown(node)
-    return nil
-  end
-  end
-
+# Exception for attempting to remove a team that doesn't exist
+class NodeDoesntExist < StandardError
 end
 
 class HeapNode
@@ -62,4 +16,76 @@ class HeapNode
     @right = right
     @parent = parent
   end
+end
+
+class SelectionHeap
+  
+  hashmap = Hash.new
+  heapRoot = nil
+  metric = nil
+  
+  #metric to organize the heap by and teams (sorted by number?) are expected to create the heap from
+  def initialize(metric, teams)
+    @metric = metric
+    teams.each_index do |i|
+      node = HeapNode.new(teams[i], @metric) 
+      addNode(node)
+    end
+  end
+
+  # removes a team from the heap and hash map, returning its score. Will return nil if an error occurs while removing the node.
+  def removeNode(number)
+    # Try to remove node from hash table
+    begin
+      node = hashMapRemove(number)
+    rescue NodeDoesntExist => e
+      puts "#{e.message}"
+      return nil
+    end
+    
+    return heapRemove(node)
+  end
+
+  # returns the team and score at the top of the heap as a tuple. returns nil if the heap is empty
+  def peek()
+    (heapRoot == nil) ? (return nil) : (return (heapRoot.team, heapRoot.score))
+  end
+
+  private
+  # Given a team number, finds the corresponding node in the hash map and returns it. returns nil if not found
+  # I think this is redundant? may be removed later
+  def lookup(number)
+    return @hashmap[number]
+  end
+
+  # adds a node to the hash map and heap
+  def addNode(node)
+    @hashmap[node.robot.number] = node
+    # TODO: add to heap itself
+  end
+
+  # helper function to handle removing things from the hashmap
+  def hashMapRemove(number)
+    ret = Hash.delete(number) # will return the node that corresponds to the number if found, or nil if not found
+    if (ret == nil)
+      raise NodeDoesntExist, "Team with number #{number} not found in the heap"
+    else return ret
+  end
+  
+  # Helper to remove node from heap after getting it from the hash table. 
+  def heapRemove(node)
+    return nil
+  end  
+
+  # reheapification for adding the given node
+  def reheapifyUp(node)
+    return nil
+  end
+
+  #reheapification for removing the given node
+  def reheapifyDown(node)
+    return nil
+  end
+  end
+
 end
